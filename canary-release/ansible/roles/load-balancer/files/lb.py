2decf11216e80012b63d9681414b43ad1f2572c6
Karan Jadhav <kjadhav@ncsu.edu>
2018-04-16 18:04:03 +0000

    added redis

--- canary-release/ansible/roles/load-balancer/files/lb.py
+++ canary-release/ansible/roles/load-balancer/files/lb.py
@@ -2,6 +2,7 @@ import BaseHTTPServer
 import time
 import sys
 import random
+import redis
 
 
 HOST_NAME = '' # !!!REMEMBER TO CHANGE THIS!!!
@@ -9,20 +10,23 @@ PORT_NUMBER = 8080 # Maybe set this to 9000.
 
 PROD = None
 STAGING = None
-
-f = open("lb.log", 'a')
+r = redis.Redis()
 
 class RedirectHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
         s.send_response(301)
 
 	r = random.uniform(0,1)
-	if r < 0.8:
-		f.write("sending to prod: "+PROD+"\n")
+	if r < 0.5:
+		print "sending to prod:", PROD
 		s.send_header("Location", "http://"+PROD)
 	else:
-		f.write("sending to staging: "+STAGING+"\n")
-		s.send_header("Location", "http://"+STAGING)
+		if r.get('flag') == '1':
+			print "sending to staging:", STAGING
+			s.send_header("Location", "http://"+STAGING)
+		else:
+			print "staging flag: FALSE, sending to PROD:", PROD
+			s.send_header("Location", "http://"+PROD)
 
         s.end_headers()
     def do_GET(s):
@@ -39,5 +43,4 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    f.close()
     #print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
