54e5dc4a996a778f6b7aca21904f7691ab8f9573
sjha5 <sjha5@ncsu.edu>
2018-04-15 20:04:47 -0400

    Automate adding IP details in heartbeat program

--- rolling-update/heartbeat/heartbeat.js
+++ rolling-update/heartbeat/heartbeat.js
@@ -1,47 +1,48 @@
-
-var http = require('http');
 var request = require('request');
 
 // websocket server that website connects to.
 var io = require('socket.io')(3000);
 
 /// CHILDREN nodes
-var nodeServers = 
-[
-	{url:"http://206.189.90.54:8080/iTrust2", latency: 0},
-	{url:"http://206.189.90.59:8080/iTrust2", latency: 0},
-];
+var nodeServers = [];
 
-function measureLatenancy(server)
-{
-	var options = 
-	{
-		url: server.url
-	};
-	request(options, function (error, res, body) 
-	{
+function setNodeServers() {
+	if (nodeServers.length == 0) {
+		var lineReader = require('readline').createInterface({
+			input: require('fs').createReadStream('../inventory-update')
+		});
+
+		lineReader.on('line', function (line) {
+			if (!line.includes("droplets_iTrust")) {
+				console.log('IP read from file:', line);
+				var server = { url: "http://" + line + ":8080/iTrust2", alive: false };
+				nodeServers.push(server);
+			}
+		});
+		console.log(nodeServers);
+	}
+}
+
+function checkResponse(server) {
+	var options =
+		{
+			url: server.url
+		};
+	request(options, function (error, res, body) {
 		error ? server.alive = false : server.alive = true;
-		console.log('Alive? ' + server.alive);
+		//console.log('Alive? ' + server.alive);
 	});
 	return server.alive;
 }
 
-function calculateColor()
-{
+function calculateColor() {
 	// latency scores of all nodes, mapped to colors.
-	var nodes = nodeServers.map( measureLatenancy ).map( function(alive) 
-	{
+	var nodes = nodeServers.map(checkResponse).map(function (alive) {
 		var color = "#ff0000";
-		if( !alive )
-		{
-			return {color: color};
-		}
-		else
-		{
+		if (alive) {
 			color = "#008000";
 		}
-		console.log( color );
-		return {color: color};
+		return { color: color };
 	});
 	//console.log( nodes );
 	return nodes;
@@ -50,23 +51,22 @@ function calculateColor()
 
 io.on('connection', function (socket) {
 	console.log("Received connection");
-
+	setNodeServers();
 	///////////////
 	//// Broadcast heartbeat over websockets
 	//////////////
-	var heartbeatTimer = setInterval( function () 
-	{
-		var data = { 
+	var heartbeatTimer = setInterval(function () {
+		var data = {
 			name: "iTrust Nodes",
 			nodes: calculateColor()
 		};
-		console.log("interval", data)
+		//console.log("interval", data)
 		//io.sockets.emit('heartbeat', data );
 		socket.emit("heartbeat", data);
 	}, 2000);
 
 	socket.on('disconnect', function () {
 		console.log("closing connection")
-    	clearInterval(heartbeatTimer);
-  	});
+		clearInterval(heartbeatTimer);
+	});
 });
